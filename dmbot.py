83733bb250abe09eab72060b1a8d1684112918e3
hs080718hs <74343433+hs080718hs@users.noreply.github.com>
2020-11-13 09:59:06 +0900

    Add files via upload

---
+++ dmbot.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from discord.ext import commands
+import discord
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(bot.user.name)
+
+def is_victoria(ctx):
+    if ctx.author.id == 765372606601494558: return True
+    return False
+
+@bot.command()
+@commands.check(is_victoria)
+@commands.guild_only()
+async def 디엠(self, ctx, *, text):
+    if "&&" not in text:
+        return await ctx.send("`!디엠 제목&&내용`")
+    fail = []
+    title = text.split("&&")[0]
+    desc = text.split("&&")[1]
+    for member in ctx.guild.members:
+        try:
+            await member.send(embed=discord.Embed(title=title, description=desc))
+        except:
+            fail.append(member.name)
+    try:
+        await ctx.send(f"""완료!
+DM 전송 실패한 사람들 (디엠을 막았을수도 있습니다) : 
+{', '.join(fail)}
+다음은 실패한 사람들의 수 입니다.
+{len(fail)}
+""")
+    except:
+        await ctx.send(f"""완료!
+DM 전송 실패한 사람들이 너무 많아 보낼 수 없습니다. 다음은 실패한 사람들의 수 입니다.
+{len(fail)}""")
+
+bot.run('Nzc2NDE3MDIzMzY5Njc0Nzcz.X60ksw.q7iqJiQjDAp5fx8vpSCJ8m2cYFs')
